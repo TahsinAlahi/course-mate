27c86f3dc038f47edd7fcb15f78e08874237226e
Tahsin Alahi <alahitahsin@gmail.com>
2025-04-23 16:11:40 +0600

    feat: types and test txt

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+// Maximum number of users, courses, and enrollments
+#define MAX_USERS 100
+#define MAX_COURSES 100
+#define MAX_ENROLLMENTS 100
+
+// Structs
+// User struct to store user information
+typedef struct {
+  char name[50];
+  char email[50];
+  char password[20];
+  char role[15]; // student/instructor
+} User;
+
+typedef struct {
+  int id;
+  char title[50];
+  char instructorEmail[50];
+} Course;
+
+typedef struct {
+  char studentEmail[50];
+  int courseId;
+} Enrollment;
+
 void showMainMenu() {
   int choice;
   do {
@@ -19,4 +44,5 @@ void showMainMenu() {
     }
   } while (choice != 0);
 }
+
 int main() { showMainMenu(); }
